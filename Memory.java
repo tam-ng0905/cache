337b74c769886c26c4b92ff98e9ca9ee0d635cbc
Tam Nguyen <ttamng0905@gmail.com>
2021-03-10 20:14:45 -0700

    initial commit

---
+++ Memory.java
@@ -0,0 +1,12 @@
+
+public interface Memory
+{
+	/**
+	 * Basic read interface to memory -- request to read a certain number of bytes starting at an address.
+	 * 
+	 * @param address
+	 * @param nBytes
+	 * @return
+	 */
+	public byte[] read(int address, int nBytes);
+}
---
+++ ReadOnlyCache.java
@@ -0,0 +1,5 @@
+
+public interface ReadOnlyCache
+{
+	public byte load(int address);
+}
